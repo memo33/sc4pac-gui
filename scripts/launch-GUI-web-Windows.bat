80059c4986bc66899a314b1c1b1ce2db82935bb7
memo <memo33@users.noreply.github.com>
2024-12-21 17:22:35 +0100

    add new `--launch-browser` option to web-app launch scripts
    Resolves #3

--- scripts/launch-GUI-web-Windows.bat
+++ scripts/launch-GUI-web-Windows.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 SET SCRIPTDIR=%~dp0.
 cd "%SCRIPTDIR%"
-cli\sc4pac.bat server --port 51515 --profiles-dir profiles --web-app-dir webapp --auto-shutdown=true %*
+cli\sc4pac.bat server --port 51515 --profiles-dir profiles --web-app-dir webapp --auto-shutdown=true --launch-browser=true %*
